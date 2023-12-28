96bd117e01520e10d3495237e6894870ad86c1b0
LucieFairePy <lesmec92@gmail.com>
2023-12-28 03:45:08 +0100

    NEW |  ShowPassword.sh - init

---
+++ .assets/plugin_showpassword.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Définir les couleurs
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+BLUE=$(tput setaf 4)
+VIOLET=$(tput setaf 5)
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+########################################## INITIALISATION ROOT ##########################################
+
+# Vérifier si l'utilisateur est root
+if [[ $EUID -ne 0 ]]; then
+   echo "${RED}${BOLD}Ce script doit être exécuté en tant que root${RESET}" 
+   # Demander le mot de passe
+   sudo "$0" "$@"
+   exit 1
+fi
+
+# Le reste du script ici
