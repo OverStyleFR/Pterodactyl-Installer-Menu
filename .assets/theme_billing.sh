1a06e8cef3b71b0917b25a3e0ee5d23b49ee6849
LucieFairePy <lesmec92@gmail.com>
2023-12-28 04:31:12 +0100

    UPDATE | Billing.sh

--- .assets/theme_billing.sh
+++ .assets/theme_billing.sh
@@ -1,148 +1,74 @@
 #!/bin/bash
 
 # Définir les couleurs
-GREEN=$(tput setaf 2)
-RED=$(tput setaf 1)
-BLUE=$(tput setaf 4)
-VIOLET=$(tput setaf 5)
-BOLD=$(tput bold)
-RESET=$(tput sgr0)
-########################################## INITIALISATION ROOT ##########################################
+# ... (restez inchangés)
 
-# Vérifier si l'utilisateur est root
-if [[ $EUID -ne 0 ]]; then
-   echo "${RED}${BOLD}Ce script doit être exécuté en tant que root${RESET}" 
-   # Demander le mot de passe
-   sudo "$0" "$@"
-   exit 1
-fi
+########################################## CHOIX D'INSTALLATION ##########################################
 
-# Le reste du script ici
+echo "${BOLD}Choisissez l'installation :${RESET}"
+echo "1. Installer le plugin 'ShowPassword'"
+echo "2. Installer le thème 'Billing' sans le plugin 'ShowPassword'"
+read -p "Entrez votre choix (1 ou 2) : " choix
 
+######################################### INSTALLATION CHOISIE ############################################
 
+if [ "$choix" == "1" ]; then
+    echo "${BOLD}Téléchargement du script d'installation du plugin 'ShowPassword'...${RESET}"
+    wget -O plugin_showpassword.sh https://raw.githubusercontent.com/OverStyleFR/Pterodactyl-Installer-Menu/V2/.assets/plugin_showpassword.sh
+    bash plugin_showpassword.sh
+elif [ "$choix" == "2" ]; then
+    # Début de l'installation du thème 'Billing' sans le plugin 'ShowPassword'
+    dossier="/tmp/pterodactylthemeinstaller"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-while true; do
-    # Affichage du menu
-    echo "                +-------------+"
-    echo "                |   Plugin :  |"
-    echo "  +-------------+------------ +--------------+"
-    echo "  | 1. Installer ShowPassword | Ptero 11.x.x |"
-    echo "  +-------------------------- +--------------+"
-    echo "  |     2. Ne pas installer ShowPassword     |"
-    echo "  +-------------------------- ---------------+"
-
-
-    # Lecture du choix de l'utilisateur
-    read -p "Choisissez une option (1-2) : " choix
-
-    # Traitement du choix
-    case $choix in
-        
-       1)
-            echo "Installation du Plugin Show Password."
-            # Ajoutez le code correspondant à l'Option 1 ici
-            bash <(curl -s https://raw.githubusercontent.com/OverStyleFR/Pterodactyl-Installer-Menu/V2/.assets/plugin_showpassword.sh)
-            ;;
-        2)
-######################################### DOWNLOAD & EXTRACT ############################################
-
-### DOSSIER TEMPORAIRE ###
-
-# Définir le chemin du dossier à vérifier
-dossier="/tmp/pterodactylthemeinstaller"
-
-# Vérifier si le dossier existe
-if [ -d "$dossier" ]; then
-    # Vérifier si le dossier est vide
-    if [ -z "$(ls -A $dossier)" ]; then
-        echo ""
-        echo "${BOLD}Le dossier existe mais est vide.${RESET}"
-        echo ""
+    if [ -d "$dossier" ]; then
+        if [ -z "$(ls -A $dossier)" ]; then
+            echo ""
+            echo "${BOLD}Le dossier existe mais est vide.${RESET}"
+            echo ""
+        else
+            rm -r "$dossier"/*
+            echo ""
+            echo "${RED}${BOLD}Le contenu du dossier a été supprimé avec succès.${RESET}"
+            echo ""
+        fi
     else
-        # Supprimer le contenu du dossier s'il n'est pas vide
-        rm -r "$dossier"/*
+        mkdir -p "$dossier"
         echo ""
-        echo "${RED}${BOLD}Le contenu du dossier a été supprimé avec succès.${RESET}"
+        echo "${GREEN}${BOLD}Le dossier a été créé avec succès.${RESET}"
         echo ""
     fi
+
+    cd /tmp/pterodactylthemeinstaller
+    echo ""
+    echo "${BOLD}Téléchargement du thème :${RESET}"
+    echo ""
+    wget -O Billing.zip https://curl.libriciel.fr/OeyR94lTmI/Billing.zip
+
+    echo ""
+    echo "${BOLD}Extraction du thème...${RESET}"
+
+    unzip Billing.zip > /dev/null 2>&1
+    echo "${BOLD}Déplacement du thème...${RESET}"
+    rsync -a --remove-source-files app config database public resources routes tailwind.config.js /var/www/pterodactyl
+    echo ""
+
+    cd /var/www/pterodactyl
+
+    echo "${BOLD}Installation de 'cross-env' via yarn...${RESET}"
+    yarn add cross-env > /dev/null 2>&1
+
+    echo "${BOLD}Mise à jour de NPX...${RESET}"
+    npx update-browserslist-db@latest > /dev/null 2>&1
+
+    echo "${BOLD}Application du thème...${RESET}"
+    php artisan billing:install stable <<< wemxgay
+
+    echo "${VIOLET}${BOLD}Re-build du thème en cours...${RESET}"
+    yarn build:production > /dev/null 2>&1
+    echo "${GREEN}${BOLD}Build Terminé !.${RESET}"
+    echo ""
+    # Fin de l'installation du thème 'Billing' sans le plugin 'ShowPassword'
 else
-    # Créer le dossier s'il n'existe pas
-    mkdir -p "$dossier"
-    echo ""
-    echo "${GREEN}${BOLD}Le dossier a été créé avec succès.${RESET}"
-    echo ""
+    echo "${RED}${BOLD}Choix invalide.${RESET}"
+    exit 1
 fi
-
-### DOWNLOAD ###
-
-cd /tmp/pterodactylthemeinstaller
-echo ""
-echo "${BOLD}Téléchargement du thème :${RESET}"
-echo ""
-wget -O Billing.zip https://curl.libriciel.fr/OeyR94lTmI/Billing.zip
-
-### EXTRACT SELECTED FILE ###
-
-echo ""
-echo "${BOLD}Extraction du thème...${RESET}"
-
-unzip Billing.zip > /dev/null 2>&1
-echo "${BOLD}Déplacement du thème...${RESET}"
-rsync -a --remove-source-files app config database public resources routes tailwind.config.js /var/www/pterodactyl
-echo ""
-
-########################################## BUILD ########################################################
-
-cd /var/www/pterodactyl
-
-## Installation cross-env
-echo "${BOLD}Installation de 'cross-env' via yarn...${RESET}"
-yarn add cross-env > /dev/null 2>&1
-
-## NPX Installation
-echo "${BOLD}Mise à jour de NPX...${RESET}"
-npx update-browserslist-db@latest > /dev/null 2>&1
-
-### APPLIQUER ###
-
-echo "${BOLD}Application du thème...${RESET}"
-php artisan billing:install stable <<< wemxgay
-
-### BUILD ###
-
-echo "${VIOLET}${BOLD}Re-build du thème en cour...${RESET}"
-yarn build:production > /dev/null 2>&1
-echo "${GREEN}${BOLD}Build Terminé !.${RESET}"
-echo ""
-            ;;
-        *)
-            echo "Choix non valide. Veuillez entrer un numéro entre 1 et 2."
-            ;;
-    esac
-done
